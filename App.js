94699d415e5246786ecc27ae3082264037bf7c74
galimto <iamgalimto@gmail.com>
2019-07-04 16:20:03 +0900

    되덜리

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , StatusBar , Dimensions, Platform } from 'react-native';
+const{height,width} = Dimensions.get("window");
+import { TextInput } from 'react-native-gesture-handler';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar barStyle="light-content"></StatusBar>
+      <Text style={styles.title}>galimto's ToDO</Text>
+      <View style={styles.card}>
+		<TextInput style={styles.input} placeholder={"새 잡"}></TextInput>
+
+      </View>
+
     </View>
   );
 }
@@ -12,8 +20,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f23657',
+    alignItems: 'center'
+    
   },
+  title:{
+    color:"white",
+    fontSize:30,
+    marginTop:50,
+	fontWeight:"200",
+	marginBottom:30
+
+  },
+  card:{
+	  flex:1,
+	  backgroundColor:"white",
+	  width:width-25,
+	  borderTopLeftRadius:10,
+	  borderTopRightRadius:10,
+	  ...Platform.select(
+		  {
+			  ios:{
+				shadowColor:"rgb(50,50,50)", 
+				shadowOpacity:0.5,
+				shadowRadius:5,
+				shadowOffset:{
+					height:-1,
+					width:0
+				}
+
+			  },
+			  android:{
+				elevatiton:3
+
+			  }
+		  }
+	  )
+	  
+  },
+  input:{
+
+  }
 });
